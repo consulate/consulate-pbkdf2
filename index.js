a04caeeaf1b638cdd829b8302f20062f58151d5f
Cameron Bytheway <cameron@nujii.com>
2013-06-19 19:25:27 -0600

    Update to the latest api

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ var pbkdf2 = require("crypto").pbkdf2;
 
 module.exports = function(options) {
   return function(app) {
-    app.hashPassword(function(password, user, done) {
+    app.verifyPassword(function(user, password, done) {
       pbkdf2(password, options.salt, options.iterations, options.keylen, function(err, hash) {
         if (err) return done(err);
-        done(null, hash.toString('hex'));
+
+        var passhash = new Buffer(user.passhash, 'hex');
+
+        done(null, safeCompare(hash, passhash));
       });
     });
   };
+};
+
+function safeCompare(a, b) {
+  // things must be the same length to compare them.
+  if (a.length != b.length) return false;
+
+  // constant-time compare
+  //   hat-tip to https://github.com/freewil/scmp for |=
+  var same = 0;
+  for (var i = 0; i < a.length; i++) {
+    same |= a[i] ^ b[i];
+  }
+  return same === 0;
 };
